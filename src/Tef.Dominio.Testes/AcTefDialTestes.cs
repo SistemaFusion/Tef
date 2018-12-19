2c399dfa9af463553a852d6ee52f10cbe1ada2be
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-12-19 15:28:24 -0200

    refatorção

--- src/Tef.Dominio.Testes/AcTefDialTestes.cs
+++ src/Tef.Dominio.Testes/AcTefDialTestes.cs
@@ -9,6 +9,17 @@ namespace Tef.Dominio.Testes
         [TestCategory("AcTefDial")]
         public void Testar_Requisicao_ATV()
         {
+            var acTefDial = CriaAcTefDial();
+
+            acTefDial.Inicializa();
+
+            var retorno = acTefDial.Atv();
+
+            Assert.AreEqual(true, retorno != null);
+        }
+
+        private static AcTefDial CriaAcTefDial()
+        {
             var requisicao = new AcTefRequisicaoFake(new ConfigRequisicao());
 
             var acTefDial = new AcTefDial(requisicao, new ConfigAcTefDial(
@@ -18,12 +29,7 @@ namespace Tef.Dominio.Testes
                 "83838",
                 false, false, false, false
             ));
-
-            acTefDial.Inicializa();
-
-            var retorno = acTefDial.Atv();
-
-            Assert.AreEqual(true, retorno != null);
+            return acTefDial;
         }
     }
 }
