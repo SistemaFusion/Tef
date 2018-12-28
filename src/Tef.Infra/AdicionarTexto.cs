060bdf5c51645890384279405d1a69aa883cbfa1
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2018-12-28 15:50:23 -0200

    adicionados fontes padrão para impressão adicionado classes para impressão de vias na impressora

---
+++ src/Tef.Infra/AdicionarTexto.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace Tef.Infra
+{
+    public class AdicionarTexto
+    {
+        public static FontFamily FontPadrao { get; set; }
+        private readonly Graphics _graphics;
+        private float _pontoX;
+        private float _pontoY;
+        private readonly SolidBrush _br;
+
+        public AdicionarTexto(Graphics graphics, string texto, int tamanhoFonte, SolidBrush br = null, Font font = null)
+        {
+            _graphics = graphics;
+            Texto = texto;
+            TamanhoFonte = tamanhoFonte;
+            _br = br;
+
+            if (font != null)
+            {
+                Fonte = font;
+            }
+
+            if (font == null)
+            {
+                Fonte = new Font(FontPadrao, TamanhoFonte);
+            }
+
+            Medida = MedidasLinha.GetMedidas(this);
+        }
+
+        public void Desenhar(float pontoX, float pontoY)
+        {
+            _pontoX = pontoX;
+            _pontoY = pontoY;
+
+            if (_br != null)
+            {
+                _graphics.DrawString(Texto, Fonte, _br, new PointF(_pontoX, _pontoY));
+                return;
+            }
+
+            _graphics.DrawString(Texto, Fonte, Brushes.Black, new PointF(_pontoX, _pontoY));
+        }
+
+        public Medida Medida { get; private set; }
+        public string Texto { get; private set; }
+        public Font Fonte { get; private set; }
+        public int TamanhoFonte { get; private set; }
+    }
+}
---
+++ src/Tef.Infra/ImprimirViaTef.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing.Printing;
+
+namespace Tef.Infra
+{
+    public class ImprimirViaTef
+    {
+        private readonly string[] _imagem;
+        private readonly string _nomeImpressora;
+        private readonly string _fontePadrao;
+        private int _tamanhoTotal;
+        private readonly int _tamanhoImagem;
+        private int _posicaoImagem;
+
+        public ImprimirViaTef(string[] imagem, string nomeImpressora, string fontePadrao = "Open Sans Condensed")
+        {
+            _imagem = imagem;
+            _nomeImpressora = nomeImpressora;
+            _fontePadrao = fontePadrao;
+            _tamanhoImagem = _imagem.Length;
+            _posicaoImagem = 0;
+        }
+
+        public void Imprimir()
+        {
+            var p = new PrintDocument { PrinterSettings = { PrinterName = _nomeImpressora } };
+
+            p.PrintPage += delegate (object sender1, PrintPageEventArgs e1)
+            {
+                var tamanhoDeUmaPagina = e1.PageSettings.PrintableArea.Height;
+
+                var graphics = e1.Graphics;
+                _tamanhoTotal = 3;
+
+                AdicionarTexto.FontPadrao = new System.Drawing.FontFamily(_fontePadrao);
+
+                var tamanhoPaginaAtual = 3;
+
+                for (; _posicaoImagem < _tamanhoImagem; _posicaoImagem++)
+                {
+                    var texto = new AdicionarTexto(graphics, _imagem[_posicaoImagem], 9);
+
+                    texto.Desenhar(3, _tamanhoTotal);
+
+                    _tamanhoTotal += texto.Medida.Altura;
+                    tamanhoPaginaAtual += 22;
+
+                    e1.HasMorePages = false;
+
+                    if (tamanhoPaginaAtual > tamanhoDeUmaPagina)
+                    {
+                        e1.HasMorePages = true;
+                        return;
+                    }
+                }
+            };
+
+            p.Print();
+        }
+    }
+}
