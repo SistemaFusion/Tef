b10c93c393be29debc859941ac7f14ee87ffd994
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-01-08 17:47:42 -0200

    refatorçaão

--- src/AppTesteTef/MainWindow.xaml.cs
+++ src/AppTesteTef/MainWindow.xaml.cs
@@ -16,6 +16,33 @@ namespace AppTesteTef
             _operadora = Operadora.Cappta;
         }
 
+        private void Atv_OnClick(object sender, RoutedEventArgs e)
+        {
+            var acTefDial = FabricaOperadora();
+            var respostaAtv = acTefDial.Atv();
+        }
+
+        private void Adm_OnClick(object sender, RoutedEventArgs e)
+        {
+            var acTefDial = FabricaOperadora();
+
+            var respostaAdm = acTefDial.Adm();
+        }
+
+        private void Cnc_OnClick(object sender, RoutedEventArgs e)
+        {
+            var acTefDial = FabricaOperadora();
+
+            var respostaAdm = acTefDial.Cnc("REDECARD", "17230215595", new DateTime(2018, 12, 04, 17, 23, 02), 50);
+        }
+
+        private void Crt_OnClick(object sender, RoutedEventArgs e)
+        {
+            var acTefDial = FabricaOperadora();
+
+            var respostaAdm = acTefDial.Crt(10m, "98393");
+        }
+
         private ITef FabricaOperadora()
         {
             var requisicao = new AcTefRequisicao(new ConfigRequisicao());
@@ -40,28 +67,15 @@ namespace AppTesteTef
             return iTef;
         }
 
-        private void Atv_OnClick(object sender, RoutedEventArgs e)
-        {
-            var acTefDial = FabricaOperadora();
-            var respostaAtv = acTefDial.Atv();
-        }
-
-        private void ExibeMensagemAction(object sender, ExibeMensagemEventArgs e)
-        {
-            Console.Out.WriteLine(e.Mensagem);
-        }
-
         private void AguardandoResposta(object sender, AguardaRespostaEventArgs e)
         {
             Console.Out.WriteLine(e.ArquivoSts);
             Console.Out.WriteLine($"Segundos: {e.Segundos}");
         }
 
-        private void Adm_OnClick(object sender, RoutedEventArgs e)
+        private void ExibeMensagemAction(object sender, ExibeMensagemEventArgs e)
         {
-            var acTefDial = FabricaOperadora();
-
-            var respostaAdm = acTefDial.Adm();
+            Console.Out.WriteLine(e.Mensagem);
         }
 
         private void ImprimirViaAction(object sender, IImprimeViaEventArgs e)
@@ -135,20 +149,6 @@ namespace AppTesteTef
                     Console.Out.WriteLine(imagemComprovante);
                 }
             }
-        }
-
-        private void Cnc_OnClick(object sender, RoutedEventArgs e)
-        {
-            var acTefDial = FabricaOperadora();
-
-            var respostaAdm = acTefDial.Cnc("REDECARD", "17230215595", new DateTime(2018, 12, 04, 17, 23, 02), 50);
-        }
-
-        private void Crt_OnClick(object sender, RoutedEventArgs e)
-        {
-            var acTefDial = FabricaOperadora();
-
-            var respostaAdm = acTefDial.Crt(10m, "98393");
         }
 
         private void AutorizaDfe(object sender, AutorizaDfeEventArgs e)
