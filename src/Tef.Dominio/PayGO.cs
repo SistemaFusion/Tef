9e859922d2019487519582c4e7265e610d72e660
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2019-01-08 10:22:22 -0200

    criado classe PayGO

---
+++ src/Tef.Dominio/PayGO.cs
@@ -0,0 +1,9 @@
+﻿namespace Tef.Dominio
+{
+    public class PayGo : AcTefDialHomologacao
+    {
+        public PayGo(IAcTefRequisicao requisicao, IConfigAcTefDial configAcTefDial) : base(requisicao, configAcTefDial)
+        {
+        }
+    }
+}
